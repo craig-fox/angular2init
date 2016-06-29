c535774fcda0144c2ea231940b9ef02e73837ed2
Craig <craig.fox.jd@icloud.com>
2016-06-30 11:45:06 +1200

    Replaced basic routing

--- app/root/app-root.ts
+++ app/root/app-root.ts
@@ -1,7 +1,11 @@
 import {Component} from 'angular2/core';
+import {ROUTER_DIRECTIVES, RouteConfig} from 'angular2/router';
 
 @Component({
     selector: 'app-root',
-    templateUrl: 'app/root/app-root.html'
+    templateUrl: 'app/root/app-root.html',
+    directives: [ROUTER_DIRECTIVES]
+    
 })
+
 export default class RootComponent {}
